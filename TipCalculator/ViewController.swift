7b8c960dcb0d023f0b6ef2c3159063315d52c7ea
kdreddy6 <89701871+kdreddy6@users.noreply.github.com>
2022-01-02 09:08:52 -0600

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Deepeswar Reddy Komma on 1/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
